a4c77373d3f5d3900d1f67a1f26744609bc13138
DataSpot42 <stevelookingforward@gmail.com>
2023-09-20 10:59:57 +0100

    removed navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <BrowserRouter>
       
         <div>
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
         <Route
             path='/'
--- frontend/src/pages/blogHome.js
+++ frontend/src/pages/blogHome.js
@@ -3,7 +3,7 @@ import './HomePage.css'
 function HomePage() {
     return (
       <div >
-     
+     <h1>Hello world</h1>
       </div>
     );
   }
