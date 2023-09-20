3f330e2852ff544430dded220fe85a1b2d309693
DataSpot42 <stevelookingforward@gmail.com>
2023-09-20 10:45:39 +0100

    started reactrouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+/* import Navbar from "./components/Navbar" */
+import HomePage from "./pages/blogHome"
 
-function App() {
+/* import Login from "./pages/Login" */
+
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+/* import './component/app.css' */
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <BrowserRouter>
+      
+        <div>
+        <Navbar />
+        <Routes>
+        <Route
+            path='/'
+            element={<HomePage />}
+          />
+         {/*  <Route
+            path='/basket/:id'
+            element={<Basket />}
+          />
+          <Route
+            path='/shop/:id'
+            element={<Shop />}
+          /> */}
+         
+        </Routes>
+        </div>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
