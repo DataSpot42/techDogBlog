d0adf0a8690206dd63d3fc0f1906db49aa8430cf
Stefan Jones <139777900+stefanthomasjones@users.noreply.github.com>
2023-09-21 11:26:41 +0100

    Tag components

---
+++ frontend/src/components/Tags.js
@@ -0,0 +1,34 @@
+import './tags.css'
+import {useState} from 'react'
+
+function Tags(){
+    const [tag, setTag] = useState([]) 
+
+function handleKeydown(e){
+      if(e.key !== 'Enter') return
+      const value = e.target.value
+      if(!value.trim()) return
+      setTag([...tag, value])
+      // Clear the input
+      e.target.value = ''
+}
+function removeTag(index){
+    setTag(tag.filter((e, i) => i !== index))
+}
+    return (
+    
+    <div className="tags-input-container">
+{tag.map((input, index)=>(
+            <div className="tag-item" key={index}>
+                <span className="text">{input}</span>
+                <span className="close"  onClick={() => removeTag(index)}>&times;</span>
+            </div>
+
+))}
+            <input onKeyDown={handleKeydown} type="text" className="tags-input" placeholder="Type somthing" />
+  </div>
+    );
+  }
+
+export default Tags
+
