8ccf91a44e504cd655e9e93283a9f8a32d909976
Stefan Jones <139777900+stefanthomasjones@users.noreply.github.com>
2023-09-22 10:16:40 +0100

    HomePage Update

--- frontend/src/pages/BlogHome.js
+++ frontend/src/pages/BlogHome.js
@@ -1,36 +1,38 @@
 import './homePage.css'
 import Logo from '../components/images/logo.png'
+import { RiLoginBoxFill } from 'react-icons/ri'
+import { TbListSearch } from 'react-icons/tb'
 function HomePage() {
-    return (
-     <div>
-      <div className="top">
-           <img className = "logo" src = {Logo} ></img>
-           <h1> WELCOME TO TECHDOG</h1>
-           <div className = "button">
-         <p className = "button1"> login </p>
-         <p className= "button2"> signup</p>
-         </div>
-      </div>
-      
-        <p className = "bg">
-           </p>
-           
-     <nav> 
-      <ul>
 
 
-       <div class="icon">
-          <li id="login-icon"><img src="./techdoglogo.png" alt="logo"></img> </li>
-              <li id="home-icon"><img src="../frontend/src/pages/techdoglogo.png" alt="logo"  width="50" height="60"/></li>
-          </div>
+  return (
+    <div className='container'>
+      <div className='nav-wrap'>
+        <ul className="nav">   
+        <li><img className="logo" src={Logo} ></img> </li>
+          <div className='nav-btns'> 
+          <li className="button2 button">    <TbListSearch /></li>
+             <li className="button1 button"> <RiLoginBoxFill /> 
+              </li>
+                  
+                   </div>
+       
+        
           </ul>
-  </nav>
+       
+      </div>
 
-
-
-  
-  </div>
-    );
-  }
+      <div className="bg">
+        <div className='hidden'>blank</div>
+        <div className='titleH'><p>WELCOME TO TECHDOG</p> </div>
+        
+        <div className='btn-block'>
+ <button className='login-btn'>Login</button>
+ </div>
+      </div>
+     
+    </div>
+  );
+}
 
 export default HomePage
