24b0435ada4f83a295b2e65d3ae7641ae3d3d58d
Hakeem Khaliq <KHA17001013@gmail.com>
2023-09-25 16:25:05 +0100

    edit blog and edit user initial pages done

---
+++ frontend/src/api/editBlog.js
@@ -0,0 +1,34 @@
+
+
+const API_URL = `http://localhost:4000`
+
+export const editBlog = async (blog) => {
+    /* console.log(punk.items[0]) */
+    // pass paramter to function
+    // create new object with 'text' key (depending on your Model)
+    let obj = { userID: blog.userID,
+        blogID: blog.blogID,
+        timeStamp: blog.timeStamp,
+        title: blog.title,
+        text: blog.text,
+        group: blog.group,
+        image: blog.image,
+        tags: blog.tags,
+        likes: blog.likes,
+        comments: blog.comments
+        }
+    console.log(obj)
+    const response = await fetch(`${API_URL}/blogs/item`,{
+        // method type?
+        method: 'PATCH',
+        // sending body, stringify data
+        body: JSON.stringify(obj),
+        // content type?
+        headers: {
+            'Content-Type' : 'application/json'
+
+        }
+    })
+    const json = await response.json()
+    return json
+}
