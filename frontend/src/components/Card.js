5f92ead9e4b118e01cba7f811f3302bfe7f9d03c
Stefan Jones <139777900+stefanthomasjones@users.noreply.github.com>
2023-09-25 15:53:55 +0100

    Card updated
    Need styling finish a bit but are fuctional

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -19,16 +19,23 @@ const Card = ({blog}) => {
             </div>
 
             <div className="detail-box">
-                <p>Title: {/*blog.title*/}</p>
-            <div className="stylehere">TEXT: So the problem is I have a jquery gallery in the top of the content body, but I don't want the gallery to have the padding around it, I mean, the white body. I want the gallery to start at the border of the body and end at the other end. {/*blog.text*/}</div>
+                <h1>Title: {/*blog.title*/}</h1>
+            <div className="cardText">TEXT: So the problem is I have a jquery gallery in the top of the content body, but I don't want the gallery to have the padding around it, I mean, the white body. I want the gallery to start at the border of the body and end at the other end. {/*blog.text*/}</div>
            
             
             </div>
             < div className="card-bottom">
+                
+
                <div className="display-profile-card">
             <img src={Profile}></img>
-               </div>
-               <div className="tags-cards"></div>
+
+            <div className="useInfo"> Joe Blogs </div>
+            </div>
+              
+
+
+               <div className="tags-cards"><p>#Computing</p></div>
             </div>
             
         </div>
