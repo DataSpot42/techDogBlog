88f8fb2957d2f8340216e58b92cbfcce38942850
DataSpot42 <stevelookingforward@gmail.com>
2023-09-25 12:20:52 +0100

    Create buttonClick.js

---
+++ frontend/src/components/buttonClick.js
@@ -0,0 +1,8 @@
+export const btnElList = document.querySelectorAll('.btn');
+
+btnElList.forEach(btnEl => {
+    btnEl.addEventListener('click', () => {
+        document.querySelector('.special')?.classList.remove('special');
+        btnEl.classList.add('special');
+    })
+})
