bec66a12ee59253e29eb6d1b275295c9a2e0b56e
DataSpot42 <stevelookingforward@gmail.com>
2023-09-27 14:08:20 +0100

    Working on MyBlogs

--- frontend/src/pages/ShowMyBlogs.js
+++ frontend/src/pages/ShowMyBlogs.js
@@ -3,7 +3,7 @@ import { UserAuth } from '../components/AuthContext';
 import { readBlogs } from "../api/readBlogs";
 
 import Card from '../components/Card'
-import {HiSearch} from 'react-icons/hi'
+import { HiSearch } from 'react-icons/hi'
 import './showAllBlogs.css'
 import Cards from '../components/Card.js'
 import { readUsers } from "../api/readUsers";
@@ -11,7 +11,8 @@ import { readUsers } from "../api/readUsers";
 
 const ShowMyBlogs = () => {
     const { logOut, user } = UserAuth();
-    console.log("Welcome to the Blogs Page")    
+    const [myUser, setMyUser] = useState();
+    console.log("Welcome to the Blogs Page")
     const [users, setUsers] = useState([]);
     /* useEffect(() => {
       const items = JSON.parse(localStorage.getItem('userName'));
@@ -20,13 +21,22 @@ const ShowMyBlogs = () => {
       }
     }, []); */
     console.log(user)
+    /* useEffect(()=> {
+        const fetchUser = async () = {
+            setMyUser(user)
+        }
 
-    const [blogs,setBlogs] = useState([])
-    let data=99
+    }) */
+   
+    
+    const [myBlogs,setMyBlogs] = useState([])
+    const [blogs, setBlogs] = useState([])
+    let data = 99
     useEffect(() => {
         const fetchBlogs = async () => {
             let data = await readBlogs() // read blogs from database   
             setBlogs(data.blog)
+            
         }
         const fetchUsers = async () => {
             let userData = await readUsers()  //read users from database
@@ -34,65 +44,75 @@ const ShowMyBlogs = () => {
         }
         fetchBlogs()
         fetchUsers()
-    }, [])   
+    }, [data])
     let aUserName = ""
-    let aUserAvatar =""
+    let aUserAvatar = ""
     console.log(blogs)
     console.log(users)
-    for (let p=0; p<blogs.length; p++){
-        for (let q=0; q<users.user.length;q++) {
+    for (let p = 0; p < blogs.length; p++) {
+        for (let q = 0; q < users.user.length; q++) {
             if (blogs[p].userID === users.user[q].userID) {
-                
+
                 console.log(aUserName, aUserAvatar)
-                Object.assign(blogs[p],{userName: users.user[q].userName}, {avatar: users.user[q].avatar})
+                Object.assign(blogs[p], { userName: users.user[q].userName }, { avatar: users.user[q].avatar })
 
                 /* console.log(blogs[p].userID)
                 console.log(users.user[q].userID) */
-                
-                
+
+
                 /* blogs[p].push(aUserName)
                 /* blogs[p].push(users.user[q].avatar) */
                 /* console.log(`I have a match!`)
                 console.log(blogs[p]) */
- 
+
             }
         }
     }
-    let myBlogs = []
-    for (let u=0;u<blogs.length; u++) {
-    
-    if (!user.uid === blogs[u].userID) { 
-        myBlogs.push(blogs[u])
+    console.log(blogs)
+    let myBlog = []
+    for (let u = 0; u < blogs.length; u++) {
+
+        if (user.uid === blogs[u].userID) {
+            myBlog.push(blogs[u])
+
+        }
     }
-}
-        
-console.log(myBlogs)
+    console.log(myBlog)
+    useEffect(() => {
+        const filterMyBlogs = async () => {
+            if (myBlog.length>1){setMyBlogs(myBlog)}
+            console.log('useEffective')
+        }
+        filterMyBlogs()
+    }, [myBlog])
+    console.log(myBlogs)
+
     return (
-        <div className="cc">               
-            <h1>Welcome {user.displayName}</h1>
-             <div id="search-container">
+        <div className="cc">
+           {/*  <h1>Welcome {user.displayName}</h1> */}
+            <div id="search-container">
                 {/* Show name from google Auth */}
-            <form id="SearchAllbg" method="get">
-                <label>
-                  { /* <button className="btn-allbg" type="submit" name="submit" className="submit" value="Search">submit</button> */}
-                <input Id="searchBar2" Name="search" type="text" className="search" placeholder="Search Our blogs..."></input>
-                
-                </label>
-            </form>
-        </div>   
-               
- <div className="all-bg-grid">
-       
-        
-        
-            {blogs ? blogs.map((blogs,index) =><div className="blogsFlex">
-    <Card blog={blogs} />
-   
-</div> ) : <p>Loading...</p> } 
-          {/* Show all blogs */}
-              </div > 
-  </div>
-       
+                <form id="SearchAllbg" method="get">
+                    <label>
+                        { /* <button className="btn-allbg" type="submit" name="submit" className="submit" value="Search">submit</button> */}
+                        <input Id="searchBar2" Name="search" type="text" className="search" placeholder="Search Our blogs..."></input>
+
+                    </label>
+                </form>
+            </div>
+
+            <div className="all-bg-grid">
+
+
+
+                {myBlogs.length>1 ? myBlogs.map((myBlogs, index) => <div className="blogsFlex">
+                    <Card myBlogs={myBlogs} />
+
+                </div>) : <p>Loading...</p>}
+                {/* Show all blogs */}
+            </div >
+        </div>
+
     )
 }
 export default ShowMyBlogs;
