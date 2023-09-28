4b0d796264fa7337cd4486e3544381a136643f62
DataSpot42 <stevelookingforward@gmail.com>
2023-09-28 14:42:44 +0100

    EditMyBlogs Update

--- frontend/src/pages/EditMyBlogs.js
+++ frontend/src/pages/EditMyBlogs.js
@@ -2,9 +2,12 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import {editBlog} from '../api/editBlog';
 import { getBlog } from "../api/getBlog";
+import { useNavigate } from "react-router-dom"
+import Tags from "../components/Tags";
 import './editMyBlogs.css';
 
 const EditMyBlogs = (blog) => {
+    const [inputValue, setInputValue] = useState('');
     console.log(blog)
 
     console.log("welcome to EditMyBlogs")
@@ -18,12 +21,58 @@ const EditMyBlogs = (blog) => {
             _id: toUpdate._id,
             text: userInput,        
         }
-       let response = await editBlog(obj)         
+       let response = await editBlog(obj,id)         
         console.log(response)
                
         
         return(obj)
     }
+    const [formData, setFormData] = useState({title: "",text: "",group: ""});
+    const [selectedOption, setSelectedOption] = useState("Networking");
+    
+
+	const  handleDropdownChange = (event) => {
+		setSelectedOption(event.target.value);
+        
+	};
+
+    const navigate = useNavigate();
+
+    /* const handlerBlogInput = async (e) => {
+        e.preventDefault() */
+
+        const handleChange = (event) => {
+            const { name, value } = event.target;
+            setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+          };
+
+        const handleSubmit =  async (e) => {
+            console.log(formData)
+            console.log(inputValue)
+            e.preventDefault();
+            
+        };   
+        const handleSave = async () => {
+            console.log(`Saved`)
+            alert(`title: ${formData.title}, text: ${formData.text}, Group: ${selectedOption} Image:${formData.image}`);
+            console.log(formData)
+            console.log(blog)
+            let timestamp = Date.now();
+            let blogObj = { 
+                userID: blog.userID,
+                blogID: 456,
+                image: formData.image,
+                
+                likes: 0,
+                title: formData.title,
+                text: formData.text,
+                timeStamp: timestamp,
+                group: selectedOption
+            }
+            let response = await editBlog(blogObj,id)  
+        }
+
+
 
     useEffect(() => {
         const fetchBlog = async () => {
@@ -69,6 +118,44 @@ const EditMyBlogs = (blog) => {
                 
                
             </div>
+            <div className = "blog">
+            <h1> Update your blog post </h1>
+            <form onSubmit={handleSubmit}>
+                <div className = "container"> 
+                <div className = "title">
+            <label  htmlFor="title">Title:</label>
+            <p></p>
+            <input className = "titlebox" type="title" id="title" name="title" value={formData.title} onChange={handleChange}/>
+            <p></p>
+            </div>
+            <div className = "select">
+            <label>
+			Select an option:
+				<select  value={selectedOption} onChange={handleDropdownChange}>
+				<option  value="Networking">Networking</option>
+				<option  value="Soft Dev">Soft Dev</option>
+				<option  value="Cloud Engineering">Cloud Engineering</option>
+                <option  value="IT Support">IT Support</option>
+                <option  value="Web Design">Web Design</option>
+                <option  value="Consumer Tech">Consumer Tech</option>
+			</select>
+		</label>
+		<p className = "select1">Selected option: {selectedOption}</p>
+        </div>
+        </div>
+      
+            <label htmlFor="text">Text:</label>
+            <p></p>
+            <textarea className = "textbox" id="text" name="text" value={formData.text} onChange={handleChange}/>
+            <p></p>
+            <label htmlFor="image">Add Image URL:</label>
+            <p></p>
+            <input id="image" className="image" value={formData.image} onChange={handleChange}/>
+            <Tags onChange={(event) => setInputValue(event.target.value)}/>
+            
+          </form>
+          <button className = "post" onClick={()=>handleSave()}>Update</button>
+          </div>
             
         </div>
         </div>
