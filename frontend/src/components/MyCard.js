baecfa0fc74e8357eee80321a52c0ba81f56d737
jessblob <jessicachau370@gmail.com>
2023-09-28 04:30:07 -0700

    editmyblog

---
+++ frontend/src/components/MyCard.js
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom"
+import './card.css'
+import Profile from './images/profile.png'
+import Networking from './images/networking.webp'
+
+
+
+const MyCard = ({blog}) => {
+    
+    
+
+    return (
+        <div className='card_item'>
+            <div className="card_top">
+                 <div className="card-img-top"> <img src={blog.image} alt="BlogImage" height="100" width="auto" ></img>{/*blog.image*/}</div>
+
+                 <div className="subjectGroup"><p>{blog.group}</p>{/**/}</div> 
+                
+                {/* <img className='card_img' src={blog.} alt="" /> */}
+            </div>
+
+            <div className="detail-box">
+                <h1>{blog.title}</h1>
+            <div className="cardText">{blog.text}</div>
+           
+            
+            </div>
+            < div className="card-bottom">
+                
+
+               <div className="display-profile-card">
+            <img src={blog.avatar}></img>
+
+            <div className="useInfo">{blog.userName}</div>
+            </div>
+              
+
+
+               <div className="tags-cards"><p>#Computing</p></div>
+               <Link className="btnLinks"
+
+                to={`EditMyBlogs/${blog._id}`}
+
+            > edit
+
+            </Link> 
+               
+            </div>
+            
+        </div>
+
+    )
+}
+export default MyCard
---
+++ frontend/src/pages/EditMyBlogs.js
@@ -0,0 +1,6 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import {editBlog} from '../api/editBlog';
+
+
+export default EditMyBlogs 
