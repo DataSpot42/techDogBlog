5a54850275f672a5cb97e87f13120ffb3026e249
DataSpot42 <stevelookingforward@gmail.com>
2023-09-28 20:03:47 +0100

    searchEnabled

--- frontend/src/components/searchBlogs.js
+++ frontend/src/components/searchBlogs.js
@@ -2,25 +2,28 @@
 
 
 
-const SearchBlogs = async (blogs, search) =>{
-   let searchArry = []
+const SearchBlogs = async (blogs, search) => {
+  let searchArry = []
   console.log(blogs)
   console.log(search)
 
-    for (let i = 0; i < blogs.length; i++){
-if(blogs[i].title.includes(search)){
-        searchArry.push(blogs[i])
-} 
-if(blogs[i].text.includes(search)){
-    searchArry.push(blogs[i])
-} 
-return searchArry
-
-
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogs[i].title.includes(search)) {
+      searchArry.push(blogs[i])
     }
+    if (blogs[i].text.includes(search)) {
+      searchArry.push(blogs[i])
+    }
+    
 
-    console.log(blogs)
+
+  }
+
+  console.log(blogs)
   console.log(search)
+  console.log('searched')
+  console.log(searchArry)
+  return searchArry
 }
 
 
